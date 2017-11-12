c9a75002ae1c840c1f369ad434cabaee16c8f3d1
joe.kocsis3@gmail.com <joe.kocsis3@gmail.com>
2017-11-12 16:30:58 -0500

    Implemented servo controls for steering

---
+++ ros/src/hardwareout/scripts/TanisTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''
+**********************************************************************
+* Filename    : tanisTest.py
+* Description : Test script for various parts of the car.
+* Author      : Joe Kocsis
+* E-mail      : Joe.Kocsis3@gmail.com
+* Website     : www.github.com/jkocsis3/tanis
+**********************************************************************
+'''
